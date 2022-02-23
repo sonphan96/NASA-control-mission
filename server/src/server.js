1792e2b82437f7b166039e5357780536d79a919c
Ethan <phantruongson96@gmail.com>
2022-02-23 11:13:06 -0500

    fetched habitable planets

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,17 @@
 const http = require('http');
 
 const app = require('./app');
+const { loadPlanetData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log(`Listening on PORT ${PORT}`);
-});
+async function startServer() {
+  await loadPlanetData();
+  server.listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}`);
+  });
+}
+
+startServer();
