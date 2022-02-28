a5221183772667728397eb34ae5210e4d68acdd8
Ethan <phantruongson96@gmail.com>
2022-02-27 20:24:09 -0500

    data access layer

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -40,7 +40,11 @@ function loadPlanetData() {
   });
 }
 
+function getAllPlanets() {
+  return habitablePlanets;
+}
+
 module.exports = {
   loadPlanetData,
-  planets: habitablePlanets,
+  getAllPlanets,
 };
