5590f0bfa21860b5e165232ef3df0b464c8d8e8a
Ethan <phantruongson96@gmail.com>
2022-03-01 20:41:18 -0500

    post launch & updated front end

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -7,7 +7,7 @@ const launch = {
   mission: 'Kepler Exploration X',
   rocket: 'Explorer IS1',
   launchDate: new Date('December 27, 2030'),
-  destination: 'Kepler-442 b',
+  target: 'Kepler-442 b',
   customer: ['NASA', 'ZTM'],
   upcoming: true,
   success: true,
@@ -19,6 +19,7 @@ function getAllLaunches() {
   return Array.from(launches.values());
 }
 
+// add new launch
 function addNewLaunch(launch) {
   latestFlightNumber++;
   launches.set(
