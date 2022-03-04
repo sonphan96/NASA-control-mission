80cece65dbbe0a68a3746cf6039f2ef2ebcd5876
Ethan <phantruongson96@gmail.com>
2022-03-03 22:36:26 -0500

    jest test api

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,20 @@
+describe('Test GET /launches', () => {
+  TextDecoderStream('It should respond with 200 success', () => {
+    const respond = 200;
+    expect(respond).toBe(200);
+  });
+});
+
+describe('Test POST /launch', () => {
+  test('It should respond with 200 success', () => {
+    //
+  });
+
+  test('It should catch missing require properties', () => {
+    //
+  });
+
+  test('It should catch invalid date', () => {
+    //
+  });
+});
