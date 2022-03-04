588d5e7af9ad36aacf35667218559be79615dfb2
Ethan <phantruongson96@gmail.com>
2022-03-03 23:26:01 -0500

    test get /launches

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,7 +1,13 @@
+const request = require('supertest');
+const app = require('../../app');
+
 describe('Test GET /launches', () => {
-  TextDecoderStream('It should respond with 200 success', () => {
-    const respond = 200;
-    expect(respond).toBe(200);
+  test('It should respond with 200 success', async () => {
+    const respond = await request(app)
+      .get('/launches')
+      .expect('Content-Type', /json/)
+      .expect(200);
+    // expect(respond.statusCode).toBe(200);
   });
 });
 
